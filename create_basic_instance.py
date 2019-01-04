f4a562ed2ddf8955b2a8dd5b8d56a5e1535c4862
pkumarbe <mymail8500@gmail.com>
2019-01-04 20:40:56 +0530

    Create create_basic_instance.py

---
+++ create_basic_instance.py
@@ -0,0 +1,9 @@
+import boto3
+ec2 = boto3.resource('ec2')
+createInstance = ec2.create_instances(
+       ImageId='ami-009d6802948d06e52',
+       MinCount=1,
+       MaxCount=1,
+       InstanceType='t2.micro',
+       KeyName='my_keypair'
+        )
